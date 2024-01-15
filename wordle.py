a6c9d86633d9a74f95dc57f2d8e05fd35266a0aa
Jaysen <jaysenstolarz@gmail.com>
2024-01-14 22:06:14 -0500

    srtcgjhkvhkj

--- wordle.py
+++ wordle.py
@@ -1,5 +1,4 @@
 import random
-import re
 
 GUESSES = 6
 
@@ -20,23 +19,50 @@ def check_letter(letter, place, word):
             return "\033[1;33;40m " + letter + " "
     return "\033[1;30;40m " + letter + " "
 
-def play_round(guess, answer, lives, valid_words, prev_guesses):
+def print_answers(guesses):
+        for i in range(len(guesses)):
+            for j in range(5):
+                print(guesses[i][j], end="\s")
+            print("\n")
+
+def play_round(guesss, answer, lives):
     letters = []
-    if guess in valid_words:
-        for i in range(5):
-            letters.append(check_letter(guess[i], i, answer))
-        for j in range(5):
-            print(letters[j] + '\s')
-        if guess == answer:
-            print()
-
-
-def print_guess(lst):
-    for letter in lst:
-        print(letter)
-
+    guess = guesss.lower()
+    for i in range(5):
+        letters.append(check_letter(guess[i], i, answer))
+    for j in range(5):
+        print(letters[j] + '\s')
+    if guess == answer:
+        return True, lives, letters
+    else:
+        lives += 1
+        return False, lives, letters
+    
 
 
 def main():
     valid_words, available_words = create_data("sgb-words.txt")
     prev_guesses = []
+    lives = 0
+    print("WELCOME TO WORDLE")
+    answer = available_words[random.randrange(0, len(available_words))]
+    while True:
+        checked = []
+        status = None
+        print("\n")
+        guess = input("Put your guess here, no spaces please! : ").split()
+        print("\n")
+        if guess in valid_words:
+            end_round = play_round(guess, answer, lives)
+            if end_round[0] == True:
+                print("Congrats! You win! you had " + str(GUESSES - lives) + " lives left, good job!")
+                break
+            elif end_round[0] == False:
+                
+
+            
+        else:
+            print("Thats not a valid word, try again!")
+            continue
+        
+
